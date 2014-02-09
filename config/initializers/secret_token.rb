4bc29f0ae1284db1699943a106f38ba43631d4b6
Matthew Duff <matthewlduff@gmail.com>
2014-02-09 16:08:40 -0700

    created users table

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+War::Application.config.secret_key_base = 'bc3c6f4b7873a7bc0564a4657c07b43b5246a6c1deb418354a09deb8d16347be9c7dbbd11c771ec66ef1cafcc9aea4139e23454b3a6e6b34a85a1319dd0c1d49'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+War::Application.config.session_store :cookie_store, key: '_war_session'
