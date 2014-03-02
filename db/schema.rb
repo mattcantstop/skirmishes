3ebdfec750bc1f80694d0f81c36b82095a466109
Matthew Duff <matthewlduff@gmail.com>
2014-03-02 08:10:04 -0700

    added authentication_token to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140301234536) do
+ActiveRecord::Schema.define(version: 20140302150903) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140301234536) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "is_disabled",            default: false
+    t.string   "authentication_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
