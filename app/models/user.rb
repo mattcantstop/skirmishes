d95295a3c4af16252b53a31c831d01c57fdc2a3e
Matthew Duff <matthewlduff@gmail.com>
2014-03-03 08:22:11 -0700

    added :login in devise config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,38 @@
 class User < ActiveRecord::Base
   include Tokenable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, :uniqueness => { :case_sensitive => false }
+
+  attr_accessor :login
+
   has_many :wars
+
+  ###
+  # Class Methods
+  ###
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  ###
+  # Instance Methods
+  ###
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
 
 end
