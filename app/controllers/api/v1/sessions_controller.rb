176bc984c0f188b5b88dffc9e6d7067909e83654
Matt Duff <matt.duff@moneydesktop.com>
2014-03-06 08:16:58 -0700

    next error to fix is the 401, but recognizing helper method now

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,18 +1,18 @@
 class Api::V1::SessionsController < Devise::SessionsController
 
-  def create
-    login = params[:username] || params[:email]
-    if login.include?('@')
-      user = User.find_by_email(login)
-    else
-      user = User.find_by_username(login)
-    end
-    if user && user.authenticate_api_v1_user
-      render 'war/show.rabl'
-    else
-      render_errors(@user)
-    end
-  end
+#  def create
+#    login = params[:username] || params[:email]
+#    if login.include?('@')
+#      user = User.find_by_email(login)
+#    else
+#      user = User.find_by_username(login)
+#    end
+#    if user && user.authenticate_api_v1_user
+#      render 'war/show.rabl'
+#    else
+#      render_errors(@user)
+#    end
+#  end
 
 
 end
