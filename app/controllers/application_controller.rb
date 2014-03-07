3671c94787d8d73e74200d0e42310c15612fbf3b
Matt Duff <matt.duff@moneydesktop.com>
2014-03-07 08:08:01 -0700

    added sessions spec and current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
 
+  before_filter :current_user
+
+  def current_user
+    @current_user ||= User.find(params[:authentication_token])
+  end
 
 end
---
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Api::V1::SessionsController do
+
+  context "an existing user accesses the app" do
+
+    it "returns an authentication token after completed authentication" do
+
+    end
+
+
+  end
+
+
+end
