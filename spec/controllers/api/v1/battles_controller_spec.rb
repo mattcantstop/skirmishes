a934a08a1e8e9eab993289ceaf126cd7017c759c
Matt Duff <matt.duff@moneydesktop.com>
2014-03-12 19:23:48 -0600

    bones of the battles spec

---
+++ spec/controllers/api/v1/battles_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Api::V1::BattlesController, :type => :request do
+
+  context "#create" do
+
+    it "responds with a 201 upon sucessful create" do
+
+    end
+
+  end
+
+end
